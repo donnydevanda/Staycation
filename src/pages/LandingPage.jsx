74bcced108dcdd717c22b3274f3aedda77085e4a
Muhammad Donny Devanda <devandadonny@gmail.com>
2021-07-31 20:33:53 +0700

    hero section, icon asset

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,14 @@
 import React, { Component, Fragment } from "react";
 import Header from "../parts/Header";
+import Hero from "../parts/Hero";
+import landingPage from "json/landingPage.json";
 
 export default class LandingPage extends Component {
   render() {
     return (
       <Fragment>
         <Header>{this.props}</Header>
+        <Hero data={landingPage.hero} />
       </Fragment>
     );
   }
