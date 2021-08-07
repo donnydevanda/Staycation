d81a4cb5b678103814ac487a9236030128ab383b
Muhammad Donny Devanda <devandadonny@gmail.com>
2021-08-07 10:50:18 +0700

    footer section

---
+++ src/parts/Footer.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+
+import Button from "components/Button";
+import IconText from "./IconText";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <IconText />
+            <p className="brand-tagline">
+              We kaboom your beauty holiday instanly and memorable.
+            </p>
+          </div>
+          <div className="col-3">
+            <h6 className="mt-2">Explore</h6>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Button type="link" href="/register">
+                  New Account
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/properties">
+                  Start Booking a Room
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/use-payments">
+                  Use Payments
+                </Button>
+              </li>
+            </ul>
+          </div>
+          <div className="col-3">
+            <h6 className="mt-2">Explore Us</h6>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Button type="link" href="/careers">
+                  Our Careers
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/privacy">
+                  Our Privacy
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/terms">
+                  Terms & Conditions
+                </Button>
+              </li>
+            </ul>
+          </div>
+          <div className="col-3">
+            <h6 className="mt-2">Connect Us</h6>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Button
+                  isExternal
+                  type="link"
+                  href="mailto:support@staycation.com"
+                >
+                  support@staycation.com
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button isExternal type="link" href="tel:+628176011117">
+                  021 - 123 -4567
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <span>Staycation, South Jakarta.</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col text-center copyrights">
+            Copyright 2021 • All rights reserved • Staycation
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
